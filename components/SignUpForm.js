4aa6356d77518c67c13cf493efc0dd5ce15de7a6
GenoDiaz <genodiaz@yahoo.com>
2018-06-15 00:28:33 -0700

    Create sign up form hook up to fire base user creation and twilio service

---
+++ components/SignUpForm.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { FormLabel, FormInput, Button } from 'react-native-elements';
+import axios from 'axios';
+
+const ROOT_URL = 'https://us-central1-one-time-password-68eb2.cloudfunctions.net';
+
+class SignUpForm extends Component {
+  state = { phone: '' };
+
+  handleSubmit = async () => {
+    try {
+      console.log('phone', this.state.phone);
+      let response = await axios.post(`${ROOT_URL}/createUser`, { phone: this.state.phone });
+      console.log('response', response);
+      await axios.post(`${ROOT_URL}/requestOneTimePassword`, { phone: this.state.phone });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  render() {
+    return (
+      <View>
+        <View style={{ marginBottom: 10 }}>
+          <FormLabel>Enter Phone Number</FormLabel>
+          <FormInput value={this.state.phone} onChangeText={phone => this.setState({ phone })}/>
+        </View>
+        <Button onPress={this.handleSubmit} title="Submit"/>
+      </View>
+    );
+  }
+}
+
+export default SignUpForm;
